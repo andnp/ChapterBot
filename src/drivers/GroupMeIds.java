1299022de850ad017102a4cf4988683afcfc09cd
andnp <soundguyandyp@gmail.com>
2015-03-19 12:26:16 -0400

    Add dynamic user-id retrieval template

---
+++ src/drivers/GroupMeIds.java
@@ -0,0 +1,11 @@
+package drivers;
+
+public class GroupMeIds {
+	public String getUserID(String name){
+		// Check userid.json for (name, id) pair
+		// If no id, then start checking Groups for ID
+		// If found in group, add to userid.json
+		
+		return "";
+	}
+}
